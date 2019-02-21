232cf12ef234d371afc3f7effe29b7f9026b931b
Rebecca Uranga <urangarebecca@gmail.com>
2019-02-21 14:56:40 -0500

    makes monster change directions every 2 seconds

--- catch-the-monster.py
+++ catch-the-monster.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 def main():
     width = 512
@@ -15,6 +16,10 @@ def main():
     background_image = pygame.image.load('images/background.png').convert_alpha()
     hero_image = pygame.image.load('images/hero.png').convert_alpha()
     monster_image = pygame.image.load('images/monster.png').convert_alpha()
+    change_dir_countdown = 120
+    change_direction = 1
+    x_dir = 3
+    y_dir = 3
 
 
     # Game initialization
@@ -28,21 +33,34 @@ def main():
             if event.type == pygame.QUIT:
                 stop_game = True
 
-
         # Game logic
-        monster_x += 5
-        monster_y += 5
-        monster_x -= 5
-        monster_y -= 5
+        # monster_x += 5
+        # monster_y += 5
+        # monster_x -= 5
+        # monster_y -= 5
 
         if monster_x > width:
             monster_x = 0
-        elif monster_y > height:
+        if monster_y > height:
             monster_y = 0
-        elif monster_x < 0:
+        if monster_x < 0:
             monster_x = width
-        elif monster_y < 0:
+        if monster_y < 0:
             monster_y = width
+
+        change_dir_countdown -= 1
+        if change_dir_countdown == 0:
+            change_dir_countdown = 120
+            change_direction = random.randint(0,3)
+            print(change_direction)
+        if change_direction == 0:
+            monster_x += x_dir
+        elif change_direction == 1:
+            monster_y += y_dir
+        elif change_direction == 2:
+            monster_x -= x_dir
+        elif change_direction == 3:
+            monster_y -= y_dir
 
         # Draw background
         screen.fill(blue_color)

c2940b191f0ca5a67d8077b31af85df0cc7382f2
Rebecca Uranga <urangarebecca@gmail.com>
2019-02-22 13:25:43 -0500

    moves hero in a Hero class

--- catch-the-monster.py
+++ catch-the-monster.py
@@ -23,8 +23,22 @@ class Monster:
         self.y -= self.y_dir
 
     # def determine_new_direction(self):
-
     
+    def render(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+class Hero: 
+    def __init__(self, image_path, x, y):
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.x = x
+        self.y = y
+        self.key_direction_x = 0
+        self.key_direction_y = 0
+
+    def move_position(self):
+        self.x += self.key_direction_x
+        self.y += self.key_direction_y
+
     def render(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
@@ -38,17 +52,12 @@ def main():
     clock = pygame.time.Clock()
     monster = Monster('images/monster.png', 100, 100, 120, 1, 5, 5)
     background_image = pygame.image.load('images/background.png').convert_alpha()
-    hero_image = pygame.image.load('images/hero.png').convert_alpha()
-    hero_pos_x = 255
-    hero_pos_y = 235
-    key_direction_y = 0
-    key_direction_x = 0
+    hero = Hero('images/hero.png', 255, 235)
     
-
     # Game initialization
 
-        
     stop_game = False
+
     while not stop_game:
 
         # Monster Movement 
@@ -80,37 +89,35 @@ def main():
     
             if event.type == pygame.KEYDOWN:
                 if event.key == KEY_DOWN:
-                    key_direction_y = 4
+                    hero.key_direction_y = 4
                 elif event.key == KEY_UP:
-                    key_direction_y = -4
+                    hero.key_direction_y = -4
                 if event.key == KEY_LEFT:
-                    key_direction_x = -4
+                    hero.key_direction_x = -4
                 elif event.key == KEY_RIGHT:
-                    key_direction_x = 4
+                    hero.key_direction_x = 4
             elif event.type == pygame.KEYUP:
                 if event.key == KEY_DOWN:
-                    key_direction_y = 0
+                    hero.key_direction_y = 0
                 elif event.key == KEY_UP:
-                    key_direction_y = 0
+                    hero.key_direction_y = 0
                 if event.key == KEY_LEFT:
-                    key_direction_x = 0
+                    hero.key_direction_x = 0
                 elif event.key == KEY_RIGHT:
-                    key_direction_x = 0
-                    
-        hero_pos_x += key_direction_x
-        hero_pos_y += key_direction_y
+                    hero.key_direction_x = 0
 
-        if event.type == pygame.QUIT:
-            stop_game = True
+            if event.type == pygame.QUIT:
+                stop_game = True
 
         # Game logic
         monster.move_and_loop(width, height)
-            # monster.determine_new_direction(1)
+        # monster.determine_new_direction(1)
+        hero.move_position()
 
-            # Game display
+        # Game display
         screen.blit(background_image, (0,0))
-        screen.blit(hero_image, (hero_pos_x, hero_pos_y))
         monster.render(screen)
+        hero.render(screen)
         pygame.display.update()
         clock.tick(60)
 
